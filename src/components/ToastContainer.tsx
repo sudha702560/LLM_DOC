8846a76034bf36900d85d6e6c26bd3fe500c57df
sudha702560 <163900171+sudha702560@users.noreply.github.com>
2025-08-04 12:22:14 +0530

    Updated ToastContainer.tsx

--- src/components/ToastContainer.tsx
+++ src/components/ToastContainer.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import Toast, { ToastProps } from './Toast';
+import { useToast } from '../contexts/ToastContext';
+import Toast from './Toast';
 
-interface ToastContainerProps {
-  toasts: ToastProps[];
-  onClose: (id: string) => void;
-}
+const ToastContainer: React.FC = () => {
+  const { toasts, removeToast } = useToast();
 
-const ToastContainer: React.FC<ToastContainerProps> = ({ toasts, onClose }) => {
+  if (!toasts || toasts.length === 0) {
+    return null;
+  }
+
   return (
     <div
       className="fixed top-4 right-4 z-50 space-y-3 max-w-md"
@@ -18,7 +20,7 @@ const ToastContainer: React.FC<ToastContainerProps> = ({ toasts, onClose }) => {
           key={toast.id}
           className="animate-in slide-in-from-right-full duration-300 ease-out"
         >
-          <Toast {...toast} onClose={onClose} />
+          <Toast {...toast} onClose={removeToast} />
         </div>
       ))}
     </div>
